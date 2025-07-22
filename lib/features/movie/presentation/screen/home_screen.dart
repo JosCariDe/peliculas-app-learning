498cd0be4aaf5f5658c0abaa1f1587839c2e11e8
joseLaptop <josenarvaezcariaga@gmail.com>
2025-07-22 14:17:15 -0500

    feat/environment variables configuradas y usadas en HomeScreen

--- lib/features/movie/presentation/screen/home_screen.dart
+++ lib/features/movie/presentation/screen/home_screen.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:peliculas_app/config/constants/environment.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: Text(Environment.theMovieDbKey),
+      ),
+    );
   }
 }
