d9229e4f18fe12947dee96f39762e84b12b7f18b
joseLaptop <josenarvaezcariaga@gmail.com>
2025-07-22 16:06:05 -0500

    hotfix/Model separado

--- lib/features/movie/data/model/movieDB/movie_moviedb.dart
+++ lib/features/movie/data/model/movieDB/movie_moviedb.dart
@@ -1,63 +1,4 @@
-
-import 'dart:convert';
-
-MovieDbResponse movieDbResponseFromJson(String str) => MovieDbResponse.fromJson(json.decode(str));
-
-String movieDbResponseToJson(MovieDbResponse data) => json.encode(data.toJson());
-
-class MovieDbResponse {
-    final Dates dates;
-    final int page;
-    final List<Result> results;
-    final int totalPages;
-    final int totalResults;
-
-    MovieDbResponse({
-        required this.dates,
-        required this.page,
-        required this.results,
-        required this.totalPages,
-        required this.totalResults,
-    });
-
-    factory MovieDbResponse.fromJson(Map<String, dynamic> json) => MovieDbResponse(
-        dates: Dates.fromJson(json["dates"]),
-        page: json["page"],
-        results: List<Result>.from(json["results"].map((x) => Result.fromJson(x))),
-        totalPages: json["total_pages"],
-        totalResults: json["total_results"],
-    );
-
-    Map<String, dynamic> toJson() => {
-        "dates": dates.toJson(),
-        "page": page,
-        "results": List<dynamic>.from(results.map((x) => x.toJson())),
-        "total_pages": totalPages,
-        "total_results": totalResults,
-    };
-}
-
-class Dates {
-    final DateTime maximum;
-    final DateTime minimum;
-
-    Dates({
-        required this.maximum,
-        required this.minimum,
-    });
-
-    factory Dates.fromJson(Map<String, dynamic> json) => Dates(
-        maximum: DateTime.parse(json["maximum"]),
-        minimum: DateTime.parse(json["minimum"]),
-    );
-
-    Map<String, dynamic> toJson() => {
-        "maximum": "${maximum.year.toString().padLeft(4, '0')}-${maximum.month.toString().padLeft(2, '0')}-${maximum.day.toString().padLeft(2, '0')}",
-        "minimum": "${minimum.year.toString().padLeft(4, '0')}-${minimum.month.toString().padLeft(2, '0')}-${minimum.day.toString().padLeft(2, '0')}",
-    };
-}
-
-class Result {
+class MovieMovieDB {
     final bool adult;
     final String backdropPath;
     final List<int> genreIds;
@@ -73,7 +14,7 @@ class Result {
     final double voteAverage;
     final int voteCount;
 
-    Result({
+    MovieMovieDB({
         required this.adult,
         required this.backdropPath,
         required this.genreIds,
@@ -90,8 +31,8 @@ class Result {
         required this.voteCount,
     });
 
-    factory Result.fromJson(Map<String, dynamic> json) => Result(
-        adult: json["adult"],
+    factory MovieMovieDB.fromJson(Map<String, dynamic> json) => MovieMovieDB(
+        adult: json["adult"] ?? 'false',
         backdropPath: json["backdrop_path"],
         genreIds: List<int>.from(json["genre_ids"].map((x) => x)),
         id: json["id"],
@@ -99,7 +40,7 @@ class Result {
         originalTitle: json["original_title"],
         overview: json["overview"],
         popularity: json["popularity"]?.toDouble(),
-        posterPath: json["poster_path"],
+        posterPath: json["poster_path"] ?? '',
         releaseDate: DateTime.parse(json["release_date"]),
         title: json["title"],
         video: json["video"],
