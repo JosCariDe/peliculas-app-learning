9daf585f096f7b21f532e7b247dd8b492444d9f4
joseLaptop <josenarvaezcariaga@gmail.com>
2025-07-22 21:56:44 -0500

    feat/ruta de home configurada para usar el provider de Bloc

--- lib/config/routes/app_router.dart
+++ lib/config/routes/app_router.dart
@@ -1,15 +1,21 @@
-
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
+import 'package:peliculas_app/di/injection.dart';
+import 'package:peliculas_app/features/movie/presentation/home/bloc/bloc/get_now_movies_bloc.dart';
 import 'package:peliculas_app/features/movie/presentation/home/screen/home_screen.dart';
 
 final appRouter = GoRouter(
   initialLocation: '/',
-  routes: [  
-
+  routes: [
     GoRoute(
       path: '/',
-      builder: (context, state) => const HomeScreen(),
-    )
-    
-  ] 
+      builder: (context, state) =>
+          MultiBlocProvider(
+            providers: [
+              BlocProvider(create: (_) => sl<GetNowMoviesBloc>()),
+            ], 
+            child: const HomeScreen()
+          ),
+    ),
+  ],
 );
