efd1b871c32d630880d017faefc67bc4f5dda3d4
joseLaptop <josenarvaezcariaga@gmail.com>
2025-07-24 09:22:23 -0500

    feat/CustomAppBar creado y agregado en el homeScreen

--- lib/features/movie/presentation/home/screen/homev2_screen.dart
+++ lib/features/movie/presentation/home/screen/homev2_screen.dart
@@ -2,18 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:peliculas_app/features/movie/domain/entities/movie.dart';
 import 'package:peliculas_app/features/movie/presentation/home/bloc/bloc/get_now_movies_bloc.dart';
+import 'package:peliculas_app/features/movie/presentation/home/widgets/widgets_barril.dart';
 
 class Homev2Screen extends StatelessWidget {
   const Homev2Screen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return _HomeView();
+    return const Scaffold(body: _HomeView());
   }
 }
 
 class _HomeView extends StatefulWidget {
-
   const _HomeView();
 
   @override
@@ -21,7 +21,6 @@ class _HomeView extends StatefulWidget {
 }
 
 class _HomeViewState extends State<_HomeView> {
-
   @override
   void initState() {
     super.initState();
@@ -30,20 +29,27 @@ class _HomeViewState extends State<_HomeView> {
 
   @override
   Widget build(BuildContext context) {
+    final List<Movie> movies =
+        context.watch<GetNowMoviesBloc>().currentMovies ?? [];
 
-    final List<Movie> movies = context.watch<GetNowMoviesBloc>().currentMovies ?? [];
+    return Column(
+      children: [
 
-    return Scaffold(
-      body: ListView.builder(
-        itemCount: movies.length,
-        itemBuilder: (context, index) {
-          final movie = movies[index];
-          return  ListTile(
-            title: Text(movie.title),
-            //subtitle: Text(movies[index].overview),
-          );
-        
-      },),
+        CustomAppBar(),
+
+        Expanded(
+          child: ListView.builder(
+            itemCount: movies.length,
+            itemBuilder: (context, index) {
+              final movie = movies[index];
+              return ListTile(
+                title: Text(movie.title),
+                //subtitle: Text(movies[index].overview),
+              );
+            },
+          ),
+        ),
+      ],
     );
   }
 }
