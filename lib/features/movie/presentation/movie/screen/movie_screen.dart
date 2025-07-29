8c1fe7f54de3752d3642171c2241192e1deb1bf9
joseLaptop <josenarvaezcariaga@gmail.com>
2025-07-29 16:12:35 -0500

    feat/Detalles de una movie agregados como un slide

--- lib/features/movie/presentation/movie/screen/movie_screen.dart
+++ lib/features/movie/presentation/movie/screen/movie_screen.dart
@@ -28,7 +28,15 @@ class MovieScreen extends StatelessWidget {
             if (state is GetMovieByIdSucces) {
               return CustomScrollView(
                 physics: const ClampingScrollPhysics(),
-                slivers: [_CustomSliverAppBar(movie: state.movie)],
+                slivers: [
+                  _CustomSliverAppBar(movie: state.movie),
+                  SliverList(
+                    delegate: SliverChildBuilderDelegate(
+                      (context, index) => _MovieDetails(movie: state.movie),
+                      childCount: 1,
+                    ),
+                  ),
+                ],
               );
             }
 
@@ -70,7 +78,7 @@ class _CustomSliverAppBar extends StatelessWidget {
                   gradient: LinearGradient(
                     begin: Alignment.topCenter,
                     end: Alignment.bottomCenter,
-                    stops: [0.7,1.0],
+                    stops: [0.7, 1.0],
                     colors: [Colors.transparent, Colors.black87],
                   ),
                 ),
@@ -82,16 +90,100 @@ class _CustomSliverAppBar extends StatelessWidget {
                 decoration: BoxDecoration(
                   gradient: LinearGradient(
                     begin: Alignment.topLeft,
-                    stops: [0.0,0.3],
-                    colors: [ Colors.black87, Colors.transparent],
+                    stops: [0.0, 0.3],
+                    colors: [Colors.black87, Colors.transparent],
                   ),
                 ),
               ),
             ),
-
           ],
         ),
       ),
+    );
+  }
+}
+
+class _MovieDetails extends StatelessWidget {
+  final Movie movie;
+  const _MovieDetails({required this.movie});
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    final textStyle = Theme.of(context).textTheme;
+    final buttonStyle = Theme.of(context).buttonTheme;
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 8),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: Image.network(
+                  movie.posterPath,
+                  width: size.width * 0.25,
+                ),
+              ),
+              SizedBox(width: 15),
+              // Envuelve el texto largo en Expanded para evitar overflow horizontal
+              Expanded(
+                flex: 3, // Dale más espacio al overview
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      movie.title,
+                      style: textStyle.bodyLarge,
+                      maxLines: 4, // Limita las líneas
+                      textAlign: TextAlign.start,
+                    ),
+                    Text(
+                      movie.overview,
+                      style: textStyle.bodyMedium,
+                      overflow: TextOverflow.ellipsis, // Trunca con "..."
+                      maxLines: 4, // Limita las líneas
+                      textAlign: TextAlign.start,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Wrap(
+                        children: [
+                          ...movie.genreIds.map((gender) => TextButton(
+                            onPressed: () {},
+                            style: ButtonStyle(
+                            ),
+                            child: Text(gender),
+                          ))
+                        ],
+                      ),
+                    ),
+                    const SizedBox(height: 100),
+                  ],
+                ),
+              ),
+              Expanded(
+                flex: 1, // Menos espacio para la fecha
+                child: Text(
+                  movie.releaseDate.toString(),
+                  style: textStyle.bodySmall?.copyWith(
+                    fontWeight: FontWeight.w500,
+                  ),
+                  overflow: TextOverflow.ellipsis,
+                  maxLines: 2,
+                  textAlign: TextAlign.end,
+                ),
+              ),
+            ],
+          ),
+        ),
+
+        //? Generos de la pelicula:
+      ],
     );
   }
 }
